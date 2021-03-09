9e6788ba2d6c957313c7ab3f1a5efc41edd980da
Arenzon <atlantidka1@gmail.com>
2021-03-09 18:28:53 +0300

    Added code

--- src/test/java/ru/netology/domain/RadioTest.java
+++ src/test/java/ru/netology/domain/RadioTest.java
@@ -1,7 +1,83 @@
 package ru.netology.domain;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-class RadioTest {
+public class RadioTest {
+    Radio radio = new Radio (10, 0, 5, 100, 0, 5);
 
+    @Test
+    public void validateChangFields() {
+        Radio radio = new Radio (10, 0, 5, 100, 0, 5);
+        assertEquals(5,radio.getCurrentRadioStation());
+    }
+
+    @Test
+    public void changeAfterLastRadioStation() {
+        Radio radio = new Radio (10, 0, 5, 100, 0, 5);
+        radio.pressNextStation();
+        assertEquals(6, radio.getCurrentRadioStation());
+    }
+
+    @Test
+    public void changeUnderInitialRadioStation() {
+        Radio radio = new Radio (10, 0, 5, 100, 0, 5);
+        radio.pressPrevStation();
+        assertEquals(4, radio.getCurrentRadioStation());
+    }
+
+    @Test
+    public void nextRadioStation() {
+        Radio radio = new Radio (10, 0, 5, 100, 0, 5);
+        radio.pressNextStation();
+        assertEquals(6, radio.getCurrentRadioStation());
+    }
+
+    @Test
+    public void prevRadioStation() {
+        Radio radio = new Radio (10, 0, 5, 100, 0, 5);
+        radio.pressPrevStation();
+        assertEquals(4, radio.getCurrentRadioStation());
+    }
+
+    @Test
+    public void afterInitialRadioStation() {
+        Radio radio = new Radio (10, 0, 5, 100, 0, 5);
+        assertEquals(10, radio.getMaxRadioStation());
+    }
+
+    @Test
+    public void underInitialRadioStation() {
+        Radio radio = new Radio (10, 0, 5, 100, 0, 5);
+        assertEquals(0, radio.getMinRadioStation());
+    }
+
+    // volume
+    @Test
+    public void volumeOverMax() {
+        Radio radio = new Radio (10, 0, 5, 100, 0, 5);
+        assertEquals(100, radio.getMaxVolume());
+    }
+
+    @Test
+    public void volumeDownUnderMin() {
+        Radio radio = new Radio (10, 0, 5, 100, 0, 5);
+        assertEquals(0, radio.getMinVolume());
+    }
+
+    @Test
+    public void plusVolume() {
+        Radio radio = new Radio (10, 0, 5, 100, 0, 5);
+        radio.pressPlusVolume();
+        assertEquals(6, radio.getCurrentVolume());
+    }
+
+    @Test
+    public void minusVolume() {
+        Radio radio = new Radio (10, 0, 5, 100, 0, 5);
+        radio.pressMinusVolume();
+        assertEquals(4, radio.getCurrentVolume());
+    }
 }
+
